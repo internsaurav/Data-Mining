b14dc7fe0a67528ed4a045f1bb4a68d1e96a4280
internsaurav <sauravks@usc.edu>
2017-09-09 01:04:53 -0700

    Read files

--- Assignment1/src/main/scala/edu/usc/sauravks/saurav_sahu_task1.scala
+++ Assignment1/src/main/scala/edu/usc/sauravks/saurav_sahu_task1.scala
@@ -6,11 +6,15 @@ import org.apache.spark.SparkConf
 object saurav_sahu_task1 {
   def main(args: Array[String]): Unit = {
     println("Starting assignment")
-    val appName = "Movie_Rating_Calc"
-    val master = "local[2]" // uses as many cores as present in local machine
-    var conf = new SparkConf().setMaster(master).setAppName("Movie_Rating_Cal")
+    val appName = "Movie_Rating_Cal"
+    val master = "local[*]" // uses as many cores as present in local machine
+    val conf = new SparkConf().setMaster(master).setAppName(appName)
     val sc = new SparkContext(conf) //spark context is the interface with cluster
-    sc.stop()
 
+    //Read the files
+      val movies_file_path = "/home/saurav/Documents/Data Mining/Assignments/CSCI-541/Assignment1/ml-1m/movies.dat"
+      val distributed_movies_file = sc.textFile(movies_file_path)
+      distributed_movies_file.collect().foreach(println)
+      sc.stop()
   }
 }
