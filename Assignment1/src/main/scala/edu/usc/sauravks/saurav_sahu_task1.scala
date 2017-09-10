a03e77b23e5ce5cbd46d6355dbdce0ee2f753b67
internsaurav <sauravks@usc.edu>
2017-09-09 20:30:40 -0700

    Produced intermediate key val pairs

--- Assignment1/src/main/scala/edu/usc/sauravks/saurav_sahu_task1.scala
+++ Assignment1/src/main/scala/edu/usc/sauravks/saurav_sahu_task1.scala
@@ -2,19 +2,51 @@ package edu.usc.sauravks
 
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
+import org.apache.spark.storage.StorageLevel
 
 object saurav_sahu_task1 {
   def main(args: Array[String]): Unit = {
-    println("Starting assignment")
+
     val appName = "Movie_Rating_Cal"
     val master = "local[*]" // uses as many cores as present in local machine
     val conf = new SparkConf().setMaster(master).setAppName(appName)
     val sc = new SparkContext(conf) //spark context is the interface with cluster
 
-    //Read the files
-      val movies_file_path = "/home/saurav/Documents/Data Mining/Assignments/CSCI-541/Assignment1/ml-1m/movies.dat"
-      val distributed_movies_file = sc.textFile(movies_file_path)
-      distributed_movies_file.collect().foreach(println)
-      sc.stop()
+    //Read the files into an RDD
+    val users_file_path = "/home/saurav/Documents/Data Mining/Assignments/CSCI-541/Assignment1/ml-1m/users.dat"
+    val ratings_file_path = "/home/saurav/Documents/Data Mining/Assignments/CSCI-541/Assignment1/ml-1m/ratings.dat"
+    val storageLevel = StorageLevel.MEMORY_ONLY
+    val dist_users_data = sc.textFile(users_file_path).persist(storageLevel)
+    val dist_ratings_data = sc.textFile(ratings_file_path).persist(storageLevel)
+
+    //key value pairs from users data
+    val UID_gender_KV = dist_users_data.map(extract_UID_gender.generate_KV_pairs)
+    val UID_movie_rating_KV = dist_ratings_data.map(extract_UID_movie_rating.generate_KV_pairs)
+    val combined_KV_pair = UID_gender_KV.join(UID_movie_rating_KV)
+    val movie_gender_rating_KV = combined_KV_pair.map(kv => kv._2).map(kv => (kv._2._1,kv._1,kv._2._2))
+    movie_gender_rating_KV.collect().foreach(println)
+
+    //clean up the memory
+    dist_ratings_data.unpersist()
+    dist_users_data.unpersist()
+    sc.stop()
+//    val test = "1::1193::5::978300760"
+//    val res = extract_UID_movie_rating.generate_KV_pairs(test)
+//    println(res)
+
+  }
+}
+
+object extract_UID_gender {
+  def generate_KV_pairs(line:String): (String,String)= {
+    val split_line =  line.split("::")
+    (split_line(0),split_line(1))
+  }
+}
+
+object extract_UID_movie_rating {
+  def generate_KV_pairs(line:String): (String,(String,String)) = {
+    val split_line =  line.split("::")
+    (split_line(0),(split_line(1),split_line(2)))
   }
 }
