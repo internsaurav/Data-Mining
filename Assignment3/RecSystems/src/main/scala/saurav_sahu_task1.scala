61da968f40be4be59271e499caa1b0d3568250b9
internsaurav <sauravks@usc.edu>
2017-10-16 02:30:08 -0700

    task 1 complete with 1.06

--- Assignment3/RecSystems/src/main/scala/saurav_sahu_task1.scala
+++ Assignment3/RecSystems/src/main/scala/saurav_sahu_task1.scala
@@ -1,5 +1,7 @@
 package recSystems
 
+import java.io.{File, PrintWriter}
+
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.recommendation.{ALS, MatrixFactorizationModel, Rating}
@@ -16,11 +18,13 @@ object saurav_sahu_task1 {
     val ratingsFilePath = args(0)
     val testDataPath = args(1)
     val sc = makeSparkContext()
-    val (trainingDataKV,testingDataKV) = extractTrainingData(sc,ratingsFilePath,testDataPath)
+    val (trainingDataKV,testingDataKV,testingGroundDataKV) = extractTrainingData(sc,ratingsFilePath,testDataPath)
     val ratingsRDD = sc.parallelize(trainingDataKV)
     val model = buildRecModel(ratingsRDD)
     val testingDataRDD = sc.parallelize(testingDataKV)
     val predictions = predictFromModesl(model,testingDataRDD)
+    printAccuracyInfo(predictions,testingGroundDataKV)
+    writeSortedOutput(predictions)
     sc.stop()
   }
 
@@ -42,9 +46,10 @@ object saurav_sahu_task1 {
       testingDataHashSet += x
     }
     val testingDataBroadcastVar = sc.broadcast(testingDataHashSet)
-    val trainingDataKV = sc.textFile(ratingsFilePath).mapPartitions(data => findTrainingAndData(data,testingDataBroadcastVar)).collect()
+    val trainingDataKV = sc.textFile(ratingsFilePath).mapPartitions(data => findTrainingData(data,testingDataBroadcastVar)).collect()
+    var testingGroundDataKV = sc.textFile(ratingsFilePath).mapPartitions(data => findTestingData(data,testingDataBroadcastVar)).collectAsMap()
     testingDataBroadcastVar.destroy()
-    (trainingDataKV,testingDataTuples)
+    (trainingDataKV,testingDataTuples,testingGroundDataKV)
   }
 
   /*
@@ -58,7 +63,7 @@ object saurav_sahu_task1 {
   /*
   * it checks if the current data is in the testing set, if not emits the data
   * */
-  def findTrainingAndData(data: Iterator[String], testingDataBroadcastVar: Broadcast[mutable.HashSet[(Int, Int)]]): Iterator[Rating] = {
+  def findTrainingData(data: Iterator[String], testingDataBroadcastVar: Broadcast[mutable.HashSet[(Int, Int)]]): Iterator[Rating] = {
     var trainingDataSet = new mutable.HashSet[Rating]()
     for (line <- data){
       val lineSplit = line.split(",")
@@ -72,15 +77,66 @@ object saurav_sahu_task1 {
     trainingDataSet.toIterator
   }
 
+  def findTestingData(data: Iterator[String], testingDataBroadcastVar: Broadcast[mutable.HashSet[(Int, Int)]]): Iterator[((Int,Int),Double)] = {
+    var trainingDataSet = new mutable.HashMap[(Int,Int),Double]()
+    for (line <- data){
+      val lineSplit = line.split(",")
+      if (lineSplit(0) != "userId"){
+        val userId = lineSplit(0).toInt
+        val movieId = lineSplit(1).toInt
+        val rating = lineSplit(2).toDouble
+        if (testingDataBroadcastVar.value.contains((userId,movieId))) trainingDataSet += (((userId,movieId),rating))
+      }
+    }
+    trainingDataSet.toIterator
+  }
+
   def buildRecModel(ratings:RDD[Rating]):MatrixFactorizationModel={
-    val rank = 5
+    val rank = 3
     val numIterations = 10
     val lambda = 0.01
     new ALS().setRank(rank).setIterations(numIterations).setLambda(lambda).run(ratings)
   }
 
   def predictFromModesl(model: MatrixFactorizationModel, testingDataRDD: RDD[(Int, Int)]) = {
-    val x = model.predict(testingDataRDD).collect()
-    println(x.mkString("\n"))
+    val x = model.predict(testingDataRDD).map{ case Rating(user, product, rate) => ((user, product), rate)}.collectAsMap()
+    x
+  }
+
+
+  def printAccuracyInfo(predictions: collection.Map[(Int, Int), Double], testingGroundDataKV: collection.Map[(Int, Int), Double]) = {
+//    var accuracyInfo = mutable.HashMap[String,Int](">=0 and <1"->0, ">=1 and <2"->0, ">=2 and <3" ->0, ">=3 and <4" ->0, ">=4" -> 0)
+    var accuracyInfo = mutable.HashMap[Int,Int](0->0, 1->0, 2 ->0, 3 ->0, 4 -> 0)
+    var sumOfSquaresOfErrors = 0.0
+    val defaultRating = 3.0
+    val numElements = testingGroundDataKV.size
+    for((k,v) <- testingGroundDataKV){
+      var err =0.0
+      if (predictions.contains(k)) err = math.abs(predictions(k)-v) else err = math.abs(defaultRating-v)
+      sumOfSquaresOfErrors += err*err
+      if (err >= 4) accuracyInfo(4) += 1 else accuracyInfo(math.floor(err).toInt) +=1
+    }
+    println(">=0 and <1: " + accuracyInfo(0))
+    println(">=1 and <2: " + accuracyInfo(1))
+    println(">=2 and <3: " + accuracyInfo(2))
+    println(">=3 and <4: " + accuracyInfo(3))
+    println(">=4: " + accuracyInfo(4))
+    println("RMSE = " + math.sqrt(sumOfSquaresOfErrors/numElements))
+  }
+
+  def writeSortedOutput(predictions: collection.Map[(Int, Int), Double]) = {
+
+    var outputFileName = "saurav_sahu_result_task1.txt"
+    //    println(frequentItemSets)
+    val file = new File(outputFileName)
+    val pw = new PrintWriter(file)
+    var sortedKeySet = mutable.SortedSet[(Int,Int)]()
+    for ((k,v) <- predictions){
+      sortedKeySet += k
+    }
+    for(x <- sortedKeySet){
+      pw.write( x._1 + "," + x._2 + ","+ predictions(x) + "\n")
+    }
+    pw.close()
   }
 }
